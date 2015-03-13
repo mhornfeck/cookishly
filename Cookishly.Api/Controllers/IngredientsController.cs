43834802b58da2cd0e407d0cb1cccc8c5e6315c4
Michael Hornfeck <mhornfeck@roughstone.com>
2015-03-13 14:49:33 -0400

    added api project

---
+++ Cookishly.Api/Controllers/IngredientsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Cookishly.Services.Contract;
+
+namespace Cookishly.Api.Controllers
+{
+    public class IngredientsController : ApiController
+    {
+        private readonly IIngredientService _ingredientService;
+
+        public IngredientsController(IIngredientService ingredientService)
+        {
+            _ingredientService = ingredientService;
+        }
+
+        public async Task<IHttpActionResult> Get(GetIngredientsArgs args)
+        {
+            var result = await _ingredientService.GetIngredientsAsync(args);
+
+            if (result.IsSuccess == false)
+            {
+                return InternalServerError();
+            }
+            
+            return Ok(result.Data);
+        }
+    }
+}
